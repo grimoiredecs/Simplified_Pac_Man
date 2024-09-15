37cf29a3c4409e041794a82dbf58fafbf7670b64
116292903+grimoiredecs@users.noreply.github.com <116292903+grimoiredecs@users.noreply.github.com>
2024-09-16 01:18:02 +0700

    first initialize ghost

---
+++ ghost.py
@@ -0,0 +1,11 @@
+import networkx
+
+
+class Ghost:
+    def __init__(self, gtype):
+        self.gtype = gtype
+
+class red:
+    def __init__(self):
+        self.gtype = "red"
+    
---
+++ screen.py
@@ -0,0 +1,13 @@
+import pygame
+import networkx as nx
+pygame.init()
+
+
+dimension = (720, 1280)
+#1 for height, 2 for width
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+colors = (BLACK, YELLOW, WHITE)
+
+
